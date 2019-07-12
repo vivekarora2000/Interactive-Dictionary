22b787f739c2f037690f3f9ec76cb4dd31522802
Vivek Arora <vivekarora2000@gmail.com>
2019-07-12 17:04:37 +0530

    Version 1.1

--- app.py
+++ app.py
@@ -7,6 +7,10 @@ new_word=u_word.lower()
 def meaning(word):
     if word in data:
         return data[word]
+    elif word.title() in data:
+        return data[word.title()]
+    elif word.upper() in data:  # in case user enters words like USA or NATO
+        return data[word.upper()]
     elif(len(get_close_matches(word,data.keys()))>0):
         match=get_close_matches(word,data.keys())
         print("Did you mean %s instead ?"%match[0].upper())
